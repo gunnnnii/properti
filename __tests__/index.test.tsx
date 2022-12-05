6656d469aeea771cfe933115946f143f1ff44d58
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2022-12-05 18:20:10 +0100

    updates tests to match the new homescreen

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,36 @@
 import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
 import Home from '@/pages/index'
+import { toRoman } from '@/core/toRoman'
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders an interactive conversion input', async () => {
     render(<Home />)
+    // since the input has type number, its role is 'spinbutton'
+    // see: https://stackoverflow.com/a/72921806
+    const input = screen.getByRole('spinbutton', { name: 'The integer:' })
+    // start by checking whether the input has been focused automatically
+    expect(input).toBe(document.activeElement)
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    // make sure the converted value is displayed on the screen
+    // (assume the toRoman function is correct. it has its own unit tests)
+    let inputValue = '123'
+    await user.keyboard(inputValue)
+    expect(input).toBe(screen.getByDisplayValue(inputValue))
+    expect(screen.getByText(toRoman(+inputValue))).toBeInTheDocument()
+    await user.clear(input)
 
-    expect(heading).toBeInTheDocument()
+    // make sure the correct messages are displayed on invalid inputs
+    inputValue = '-10'
+    await user.keyboard(inputValue)
+    expect(input).toBe(screen.getByDisplayValue(inputValue))
+    expect(screen.getByText('The number is too small.')).toBeInTheDocument()
+    await user.clear(input)
+
+    inputValue = '4000'
+    await user.keyboard(inputValue)
+    expect(input).toBe(screen.getByDisplayValue(inputValue))
+    expect(screen.getByText('The number is too large.')).toBeInTheDocument()
+    await user.clear(input)
   })
 })

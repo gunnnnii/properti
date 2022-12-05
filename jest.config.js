4f9d26114806d0d3e8dba8899be025d420ab6ebd
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2022-12-05 17:28:51 +0100

    changes some configuration so the utilities folder can be resolved in the same way as other top level folders

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/utilities/(.*)$': '<rootDir>/utilities/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }

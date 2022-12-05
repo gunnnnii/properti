ac8bf1359241b50f86276796cc7e852041745333
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2022-12-05 18:19:56 +0100

    sets up the converter component and adjusts the home screens markup to fit the assignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,58 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from '@/pages/index.module.css'
+import { useMemo, useState } from 'react';
+import { MAX_SAFE_ROMAN_VALUE, MIN_SAFE_ROMAN_VALUE, toRoman } from '@/core/toRoman';
+
+const NumeralInput = () =>{
+  const [value, setValue] = useState<number | null>(null)
+  const numerals = useMemo(() => {
+    const v = value ?? 0
+    if (v < MIN_SAFE_ROMAN_VALUE) return 'The number is too small.'
+    if (v > MAX_SAFE_ROMAN_VALUE) return 'The number is too large.';
+    return toRoman(v)
+  }, [value]);
+
+  return (
+    <div className={styles.converter}>
+      <label htmlFor='number'>The integer:</label>
+      <input
+        autoFocus
+        id="number"
+        className={styles.input}
+        value={value ?? ''}
+        placeholder="123"
+        min={MIN_SAFE_ROMAN_VALUE}
+        max={MAX_SAFE_ROMAN_VALUE}
+        onChange={e => {
+          const value = e.target.value;
+          if (value) setValue(+value)
+          // this allows the input to be completely empty
+          // otherwise there will always be at least a 0 stuck
+          else setValue(null)
+        }}
+        type="number"
+      />
+      <p>The result:</p>
+      <p aria-live="polite">{numerals}</p>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Romanumeral</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>Int ➡ Roman</h1>
+        <p>Here you can convert integers between 0 and 3999 into <Link href="https://en.wikipedia.org/wiki/Roman_numerals">Roman numerals</Link></p>
+        <NumeralInput />
       </main>
 
       <footer className={styles.footer}>

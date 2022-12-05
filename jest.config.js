6ddbaff006f1255b0960992f5d2e94af497d5658
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2022-12-05 17:35:05 +0100

    renames `utilities` to `core`. it's lacking descriptiveness in the same way, but at least this way it's clear that it's important.

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/utilities/(.*)$': '<rootDir>/utilities/$1',
+    '^@/core/(.*)$': '<rootDir>/core/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
